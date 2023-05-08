6abfebbc2cce144fcb9cf81c3856ad2a07139f89
Felipe1501 <felipebsantos1501@gmail.com>
2023-05-08 14:16:27 -0300

    aprendendo a utilizar components

--- prjAngular/src/app/app.module.ts
+++ prjAngular/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { myMenu } from './components/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    myMenu
   ],
   imports: [
     BrowserModule,
