7cc2a33d0a85cde75b54b5e3eda216cb1890b88f
Felipe1501 <felipebsantos1501@gmail.com>
2023-05-29 12:32:40 -0300

    injetando um service

--- servicesAngular/src/app/components/card/card.component.ts
+++ servicesAngular/src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -11,10 +12,14 @@ export class CardComponent implements OnInit {
     name:string = ""
     attributesTypes:string[] = ['']
 
-    constructor(){}
+    constructor(
+        private service:PokemonService
+    ){
+
+    }
     
     ngOnInit(): void {
-
+        this.service.getPokemon("nome")
     }
 
 }
